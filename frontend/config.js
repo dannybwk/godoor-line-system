4092d728c0081228ca344c7aa7365e8719aa982c
dannybwk <smallbe@gmail.com>
2025-06-23 00:32:22 +0800

    Update and rename config.js.template to config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 // config.js.template
 
 const CONFIG = {
-  LIFF_ID: 'YOUR_LIFF_ID_HERE',
+  LIFF_ID: '2007618883-ewZnj9Py',
   API_BASE: 'YOUR_API_ENDPOINT_BASE_URL', // e.g., https://script.google.com/macros/s/XXX/exec
 };
