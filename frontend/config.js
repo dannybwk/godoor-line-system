d60d6532ad7795a522325ed2871da67d5e68d561
dannybwk <smallbe@gmail.com>
2025-06-23 00:33:12 +0800

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -2,5 +2,5 @@
 
 const CONFIG = {
   LIFF_ID: '2007618883-ewZnj9Py',
-  API_BASE: 'YOUR_API_ENDPOINT_BASE_URL', // e.g., https://script.google.com/macros/s/XXX/exec
+  API_BASE: 'https://script.google.com/macros/s/AKfycbxjj1HDZsxoKhs-YledJKfH4llPDEFv2WvVjomrjFQhOEyU6lWzX9hUfQYgyzcXevP_Bw/exec', // e.g., https://script.google.com/macros/s/XXX/exec
 };
